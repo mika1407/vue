7776fb54bb9fb19af3c3368465a4e796f39541f2
mika1407 <koneautomaatio@hotmail.com>
2025-08-11 05:35:18 +0300

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// tailwind.config.js
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+        fontFamily: {
+            sans: ['Poppins', 'sans-serif'],
+        },
+        gridTemplateColumns: {
+           '70/30': '70% 28%',
+        },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
