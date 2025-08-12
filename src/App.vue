37abb4918d0310d6ddd7bbb6ad15fe329a01d70c
mika1407 <koneautomaatio@hotmail.com>
2025-08-12 03:05:27 +0300

    cards added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
   import Navbar from './components/Navbar.vue';
   import Hero from './components/Hero.vue';
+  import HomeCards from './components/HomeCards.vue';
 </script>
 
 <template>
   <div>
     <Navbar />
     <Hero />
+    <HomeCards />
   </div>
 </template>
