71240a2972b58d0bec352c6ab3a62bf7f2b6e8db
mika1407 <koneautomaatio@hotmail.com>
2025-08-19 14:45:10 +0300

    Not Found Page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import JobsView from "@/views/JobsView.vue";
+import NotFoundView from "@/views/NotFoundView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,12 +9,17 @@ const router = createRouter({
         {
         path: "/",
         name: "home",
-        component: HomeView
+        component: HomeView,
         },
         {
         path: "/jobs",
         name: "jobs",
-        component: JobsView
+        component: JobsView,
+        },
+        {
+        path: '/:catchAll(.*)',
+        name: 'not-found',
+        component: NotFoundView,
         }
     ]
 });
